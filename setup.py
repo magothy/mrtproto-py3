19f184dc0eb8d4877ea0483d7978166a287df663
Nathan Knotts <nathan@magothyrt.com>
2024-02-16 08:27:53 -0500

    skeleton project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mrtproto",
+    version="0",
+    python_requires='>=3.6',
+    packages=['mrtproto'],
+    install_requires=['python3-protobuf']
+)
